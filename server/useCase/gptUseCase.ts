56332cfda07647312172a9f0399b3d463b65f205
s1f102100793 <s1f102100793@iniad.org>
2023-08-30 17:05:30 +0900

    GPTにつくってもらったのを表示

--- server/useCase/gptUseCase.ts
+++ server/useCase/gptUseCase.ts
@@ -1,5 +1,11 @@
 import { fetchNews } from '$/repository/newsRepository';
+import { OPENAIAPI } from '$/service/envValues';
+import { OpenAI } from 'langchain/llms';
 
-export const makeNews = (name: string) => {
-  return fetchNews(name);
+export const makeNews = async (name: string) => {
+  const news = await fetchNews(name);
+  console.log(news);
+  const llm = new OpenAI({ openAIApiKey: OPENAIAPI, temperature: 0.9, modelName: 'gpt-4' });
+  const res = await llm.call(`${news}の内容をまとめ${name}新たな新聞記事の内容を作成してください`);
+  return res;
 };

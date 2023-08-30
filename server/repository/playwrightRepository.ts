0baacfce07021d31de30b4307b81acbac457d4be
s1f102100793 <s1f102100793@iniad.org>
2023-08-30 23:54:22 +0900

    ついにpageを開けたアツいぜ

--- server/repository/playwrightRepository.ts
+++ server/repository/playwrightRepository.ts
@@ -17,15 +17,24 @@ export const getNewsFromGoogleSearch = async (searchQuery: string) => {
 
   await page.getByRole('link', { name: 'ニュース', exact: true }).click();
 
-  // await page.waitForSelector('article h3');
-  // 最初のニュースリンクをクリック
-  const firstNewsLink = await page.$('article h3 a');
-  if (firstNewsLink) {
-    await firstNewsLink.click();
-    await page.waitForLoadState('load'); // 新しいページがロードされるのを待機
+  await page.waitForSelector('div.n0jPhd.ynAwRc.MBeuO.nDgy9d[role="heading"]');
+  // // 最初のニュースリンクをクリック
+  // const firstNewsLink = await page.$('div n0jPhd ynAwRc MBeuO nDgy9d a');
+  // if (firstNewsLink) {
+  //   await firstNewsLink.click();
+  //   await page.waitForLoadState('load'); // 新しいページがロードされるのを待機
+  // } else {
+  //   console.log('ニュースリンクが見つかりませんでした。');
+  // }
+  const newsHeading = await page.$('div.n0jPhd.ynAwRc.MBeuO.nDgy9d[role="heading"]');
+  if (newsHeading) {
+    await newsHeading.click();
+    await page.waitForLoadState('load');
   } else {
-    console.log('ニュースリンクが見つかりませんでした。');
+    console.log('見出しが見つかりませんでした。');
   }
+
+  // <div class="n0jPhd ynAwRc MBeuO nDgy9d" aria-level="3" role="heading" style="-webkit-line-clamp:3">Scraping the unscrapable in Python using Playwright</div>
 
   // // セレクタを使用して要素を取得
   // const headingElement = await page.$('.n0jPhd.ynAwRc.MBeuO.nDgy9d');

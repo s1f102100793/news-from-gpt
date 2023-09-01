74c22711c10f4ab67c89955bba9118c33ea6d153
s1f102100793 <s1f102100793@iniad.org>
2023-09-01 18:21:36 +0900

    線を追加

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,13 +1,14 @@
 // Header.tsx
-
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
+      <div className={styles.headerLine} />
       <h1 className={styles.headerTitle}>
         GPT<span className={styles.yellowText}>N</span>EWS
       </h1>
+      <div className={styles.headerLine} />
     </div>
   );
 };

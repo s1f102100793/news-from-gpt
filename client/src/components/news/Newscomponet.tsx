4b4bba9205a2d1f0288ff01939f1912277a6491f
s1f102100793 <s1f102100793@iniad.org>
2023-09-03 01:20:41 +0900

    タイトルのサイズ変更

--- client/src/components/news/Newscomponet.tsx
+++ client/src/components/news/Newscomponet.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './news.module.css';
 
 interface NewsProps {
@@ -8,11 +8,57 @@ interface NewsProps {
 }
 
 const NewsComponent: React.FC<NewsProps> = ({ title, subtitle, body }) => {
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const adjustFontSize = () => {
+      const newsTitle = titleRef.current;
+      const newsContainer = containerRef.current;
+
+      if (!newsTitle || !newsContainer) return;
+
+      const targetWidth = newsContainer.offsetWidth * 0.8;
+      const fontSize = parseFloat(window.getComputedStyle(newsTitle).fontSize);
+
+      if (newsTitle.offsetWidth > targetWidth) {
+        // Reduce font size proportionally
+        const scaleFactor = targetWidth / newsTitle.offsetWidth;
+        newsTitle.style.fontSize = `${fontSize * scaleFactor}px`;
+      } else if (newsTitle.offsetWidth < targetWidth) {
+        // Increase font size proportionally
+        const scaleFactor = targetWidth / newsTitle.offsetWidth;
+        newsTitle.style.fontSize = `${fontSize * scaleFactor}px`;
+      }
+    };
+
+    adjustFontSize();
+
+    window.addEventListener('resize', adjustFontSize);
+
+    return () => {
+      window.removeEventListener('resize', adjustFontSize);
+    };
+  }, [title]);
+
+  const [fontSize, setFontSize] = useState('2em'); // デフォルトフォントサイズをセット
+
+  useEffect(() => {
+    const calculatedSize = `calc(2em + ${(2 * window.innerWidth) / 100}vw)`;
+    setFontSize(calculatedSize);
+  }, []);
+  
+  const subtitleMarginTop = `calc(${fontSize} * 1.5)`;
+
   return (
     <div className={styles.centerContainer}>
-      <div className={styles.newsContainer}>
-        <h1 className={styles.newsTitle}>{title}</h1>
-        <h2 className={styles.newsSubtitle}>{subtitle}</h2>
+      <div className={styles.newsContainer} ref={containerRef}>
+        <h1 className={styles.newsTitle} ref={titleRef}>
+          {title}
+        </h1>
+        <h2 className={styles.newsSubtitle} style={{ marginTop: subtitleMarginTop }}>
+          {subtitle}
+        </h2>
         <p className={styles.newsBody}>{body}</p>
       </div>
     </div>

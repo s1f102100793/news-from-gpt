e9281bb6de024ad7d32be2cedcbe07519e0da102
s1f102100793 <s1f102100793@iniad.org>
2023-09-06 02:45:44 +0900

    記事数順にソートは出来る

--- client/src/components/namelist/Namelist.tsx
+++ client/src/components/namelist/Namelist.tsx
@@ -65,10 +65,37 @@ const NameListComponent: React.FC<NameListComponentProps> = ({
     }
   };
 
+  const toHiragana = (str: string): string => {
+    // カタカナをひらがなに変換
+    return str.replace(/[\u30a1-\u30f6]/g, (match) => {
+      const chr = match.charCodeAt(0) - 0x60;
+      return String.fromCharCode(chr);
+    });
+  };
+
+  const normalizeForSort = (str: string): string => {
+    let normalized = '';
+    for (const char of str) {
+      if (/^[ァ-ン]$/g.test(char)) {
+        // カタカナの場合
+        normalized += toHiragana(char);
+      } else {
+        normalized += char;
+      }
+    }
+    return normalized;
+  };
+
+  const compareNames = (a: string, b: string): number => {
+    const normalizedA = normalizeForSort(a);
+    const normalizedB = normalizeForSort(b);
+    return normalizedA.localeCompare(normalizedB);
+  };
+
   const sortNames = (names: [string, number][]) => {
     if (sortType === 'alphabetical') {
       return names.sort((a, b) =>
-        isAscending ? a[0].localeCompare(b[0]) : b[0].localeCompare(a[0])
+        isAscending ? compareNames(a[0], b[0]) : compareNames(b[0], a[0])
       );
     } else if (sortType === 'count') {
       return names.sort((a, b) => (isAscending ? a[1] - b[1] : b[1] - a[1]));
@@ -85,12 +112,6 @@ const NameListComponent: React.FC<NameListComponentProps> = ({
 
       {selectedName === null &&
         sortedNames.map(([name, count]) => (
-          <div key={name} onClick={() => handleNameClick(name)} className={styles.nameItem}>
-            {name} ({count})
-          </div>
-        ))}
-      {selectedName === null &&
-        Array.from(nameCounts.entries()).map(([name, count]) => (
           <div key={name} onClick={() => handleNameClick(name)} className={styles.nameItem}>
             {name} ({count})
           </div>

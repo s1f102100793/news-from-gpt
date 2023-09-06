aed487cd8c868fc66e799620a303d2e0c40e7275
s1f102100793 <s1f102100793@iniad.org>
2023-09-06 20:58:52 +0900

    カスタムフックスを作成

--- client/src/hooks/useNamelist.tsx
+++ client/src/hooks/useNamelist.tsx
@@ -1,4 +1,6 @@
+import type { NewsModel } from 'commonTypesWithClient/models';
 import { useState } from 'react';
+import { apiClient } from 'src/utils/apiClient';
 
 export const useNamelist = () => {
   const [selectedName, setSelectedName] = useState<string | null>(null);
@@ -7,5 +9,85 @@ export const useNamelist = () => {
     setSelectedName(null);
   };
 
-  return { selectedName, setSelectedName, resetSelectedName };
+  const [newsData, setNewsData] = useState<NewsModel[]>([]);
+  const [sortType, setSortType] = useState<'alphabetical' | 'count' | null>(null);
+  const [isAscending, setIsAscending] = useState(true);
+
+  const fetchNews = async () => {
+    const fethedAllnews = await apiClient.news.$get();
+    return fethedAllnews;
+  };
+
+  const countNames = (data: NewsModel[]): Map<string, number> => {
+    const counts = new Map<string, number>();
+    data.forEach((item) => {
+      const currentCount = counts.get(item.name);
+      if (typeof currentCount === 'number') {
+        counts.set(item.name, currentCount + 1);
+      } else {
+        counts.set(item.name, 1);
+      }
+    });
+    return counts;
+  };
+
+  const nameCounts = countNames(newsData);
+
+  const toggleSort = (type: 'alphabetical' | 'count') => {
+    if (sortType === type) {
+      setIsAscending(!isAscending);
+    } else {
+      setSortType(type);
+    }
+  };
+
+  const toHiragana = (str: string): string => {
+    return str.replace(/[\u30a1-\u30f6]/g, (match) => {
+      const chr = match.charCodeAt(0) - 0x60;
+      return String.fromCharCode(chr);
+    });
+  };
+
+  const compareNames = (a: string, b: string): number => {
+    const hiraganaA = toHiragana(a);
+    const hiraganaB = toHiragana(b);
+    return hiraganaA.localeCompare(hiraganaB);
+  };
+
+  const sortNames = (names: [string, number][]) => {
+    if (sortType === 'alphabetical') {
+      return names.sort((a, b) =>
+        isAscending ? compareNames(a[0], b[0]) : compareNames(b[0], a[0])
+      );
+    } else if (sortType === 'count') {
+      return names.sort((a, b) => (isAscending ? a[1] - b[1] : b[1] - a[1]));
+    }
+    return names; // no sort
+  };
+
+  const sortedNames = sortNames(Array.from(nameCounts.entries()));
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return {
+    selectedName,
+    setSelectedName,
+    resetSelectedName,
+    newsData,
+    setNewsData,
+    sortType,
+    setSortType,
+    isAscending,
+    setIsAscending,
+    fetchNews,
+    nameCounts,
+    toggleSort,
+    sortedNames,
+    searchTerm,
+    handleSearchChange,
+  };
 };
